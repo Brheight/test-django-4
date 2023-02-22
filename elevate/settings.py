5e7710a96307cde255f56bfcf330754fcf5c5d8a
Bright Adewole <54893652+Brheight@users.noreply.github.com>
2023-02-22 12:50:55 +0100

    vercel update

--- elevate/settings.py
+++ elevate/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+MEDIA_URL = '/media/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main/static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
